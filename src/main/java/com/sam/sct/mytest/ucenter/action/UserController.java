7111078cf2fb5cd045102ea9aa493198c5a4e74d
zhangshexin <zhangshexin@anne.com.cn>
2019-04-17 17:44:02 +0800

    1、增加api注释

--- src/main/java/com/sam/sct/mytest/ucenter/action/UserController.java
+++ src/main/java/com/sam/sct/mytest/ucenter/action/UserController.java
@@ -3,6 +3,7 @@ package com.sam.sct.mytest.ucenter.action;
 import com.sam.sct.mytest.entity.User;
 import com.sam.sct.mytest.ucenter.service.UserService;
 import com.sam.sct.mytest.util.ResultUtile;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,46 +19,52 @@ public class UserController {
 
     /**
      * 用户登录
+     *
      * @param phoneNum
      * @param pwd
      * @return
      */
-    @RequestMapping(value = "/login",method = RequestMethod.POST)
-   public Object login(@RequestParam(value = "phoneNum",required = true)String phoneNum,@RequestParam(value = "pwd",required = true)String pwd){
-        User user=new User();
+    @ApiOperation(value = "用户登录", notes = "用户登录")
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public Object login(@RequestParam(value = "phoneNum", required = true) String phoneNum, @RequestParam(value = "pwd", required = true) String pwd) {
+        User user = new User();
         user.setPhoneNumber(phoneNum);
         user.setPwd(pwd);
-       return ResultUtile.result(ResultUtile.SUCCESS ,null,userService.findUserByUser(user));
-   }
+        return ResultUtile.result(ResultUtile.SUCCESS, null, userService.findUserByUser(user));
+    }
 
     /**
      * 用户注册
+     *
      * @param phoneNum
      * @param pwd
      * @return
      */
-    @RequestMapping(value = "/register",method = RequestMethod.POST)
-   public Object register(@RequestParam(value = "phoneNum",required = true)String phoneNum,@RequestParam(value = "pwd",required = true)String pwd){
-        User user=new User();
+    @ApiOperation(value = "用户注册", notes = "用户注册")
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
+    public Object register(@RequestParam(value = "phoneNum", required = true) String phoneNum, @RequestParam(value = "pwd", required = true) String pwd) {
+        User user = new User();
         user.setPwd(pwd);
         user.setPhoneNumber(phoneNum);
-        User exist=userService.findUserByPhoneNum(phoneNum);
-        if(exist!=null)
-            return ResultUtile.result(ResultUtile.USER_EXIST,"用户已存在",null);
+        User exist = userService.findUserByPhoneNum(phoneNum);
+        if (exist != null)
+            return ResultUtile.result(ResultUtile.USER_EXIST, "用户已存在", null);
         user.setCreateDate(new Date());
         user.setUpdateDate(new Date());
-        return ResultUtile.result(ResultUtile.SUCCESS,"注册完成",userService.saveUser(user));
-   }
+        return ResultUtile.result(ResultUtile.SUCCESS, "注册完成", userService.saveUser(user));
+    }
 
     /**
      * 获取所有用户
+     *
      * @param pageNum
      * @param pageSize
      * @return
      */
-    @RequestMapping(value = "/findAll/{pageNum}/{pageSize}",method = RequestMethod.GET )
-    public Object findAll(@PathVariable("pageNum") int pageNum,@PathVariable("pageSize") int pageSize){
-        return ResultUtile.result(ResultUtile.SUCCESS,null,userService.findAllUser( pageNum,pageSize));
+    @ApiOperation(value = "获取所有用户", notes = "获取所有用户")
+    @RequestMapping(value = "/findAll/{pageNum}/{pageSize}", method = RequestMethod.GET)
+    public Object findAll(@PathVariable("pageNum") int pageNum, @PathVariable("pageSize") int pageSize) {
+        return ResultUtile.result(ResultUtile.SUCCESS, null, userService.findAllUser(pageNum, pageSize));
     }
 
 }
