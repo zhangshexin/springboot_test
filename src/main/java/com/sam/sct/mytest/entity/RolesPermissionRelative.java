991099535b5c4dbd2615fa2aaabbe23843468ca1
zhangshexin <zhangshexin@anne.com.cn>
2019-05-22 13:43:08 +0800

    添加shiro

---
+++ src/main/java/com/sam/sct/mytest/entity/RolesPermissionRelative.java
@@ -0,0 +1,33 @@
+package com.sam.sct.mytest.entity;
+
+public class RolesPermissionRelative {
+    private Integer id;
+
+    private String permissionId;
+
+    private String roleId;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getPermissionId() {
+        return permissionId;
+    }
+
+    public void setPermissionId(String permissionId) {
+        this.permissionId = permissionId == null ? null : permissionId.trim();
+    }
+
+    public String getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(String roleId) {
+        this.roleId = roleId == null ? null : roleId.trim();
+    }
+}
